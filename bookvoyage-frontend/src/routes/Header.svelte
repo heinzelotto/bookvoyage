0bc5c6ab359fd6ae6c14db315075a205090ff201
Felix Monninger <felix.monninger@gmail.com>
2024-07-07 16:39:34 +0200

    clean up unnecessary stuff

--- bookvoyage-frontend/src/routes/Header.svelte
+++ bookvoyage-frontend/src/routes/Header.svelte
@@ -17,9 +17,6 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
 			<li aria-current={$page.url.pathname === '/list' ? 'page' : undefined}>
 				<a href="/list">List</a>
 			</li>
